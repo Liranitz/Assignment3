bd7402d8fcdbeb7b9ed3ccb27df5cbefb52ec272
Liranitz <liranitz@post.bgu.ac.il>
2022-06-19 15:19:43 +0300

    adding files

--- Assignment3/src/Tiles/Tile.java
+++ Assignment3/src/Tiles/Tile.java
@@ -2,9 +2,12 @@ package Tiles;
 
 import Tiles.Units.Unit;
 
+import Tiles.Units.Player;
+
 public abstract class Tile implements Comparable<Tile> {
     protected char tile;
     protected Position position;
+    private messageCallback messageCallback;
 
     protected Tile(char tile){
         this.tile = tile;
@@ -37,4 +40,20 @@ public abstract class Tile implements Comparable<Tile> {
     public String toString() {
         return String.valueOf(tile);
     }
+    public int defend(){
+        int result = r.nextInt(defense);
+        messageCallback.send(String.format("%s rolled %d defence points." , getName(),result));
+        return result;
+    }
+    public abstract void onDeath();
+    public void visit(Empty e){
+        swapPosition(e);
+    }
+
+    public void Visit(Wall w){
+        return;
+    }
+
+
+
 }
